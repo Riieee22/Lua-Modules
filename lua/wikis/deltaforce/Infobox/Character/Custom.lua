ba88c16b11a395bf89af6f7673d14d5dcc31dd6a
Riieee <88617169+Riieee22@users.noreply.github.com>
2026-07-11 16:10:46 +0700

    Refactor Custom.lua to include PositionIcon and Html

--- lua/wikis/deltaforce/Infobox/Character/Custom.lua
+++ lua/wikis/deltaforce/Infobox/Character/Custom.lua
@@ -9,10 +9,14 @@ local Lua = require('Module:Lua')
 
 local Class = Lua.import('Module:Class')
 
-local Injector = Lua.import('Module:Widget/Injector')
 local Character = Lua.import('Module:Infobox/Character')
+local Injector = Lua.import('Module:Widget/Injector')
+local PositionIcon = Lua.import('Module:PositionIcon', {loadData = true})
 
 local Widgets = Lua.import('Module:Widget/All')
+local Html = Lua.import('Module:Widget/Html')
+local IconImageWidget = Lua.import('Module:Widget/Image/Icon/Image')
+local Cell = Widgets.Cell
 
 ---@class DeltaforceCharacterInfobox: CharacterInfobox
 local CustomCharacter = Class.new(Character)
@@ -21,12 +25,46 @@ local CustomCharacter = Class.new(Character)
 local CustomInjector = Class.new(Injector)
 
 ---@param frame Frame
----@return VNode
+---@return Html
 function CustomCharacter.run(frame)
 	local character = CustomCharacter(frame)
 	character:setWidgetInjector(CustomInjector(character))
 	character.args.informationType = 'Characters'
 	return character:createInfobox()
+end
+
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	if id == 'role' then
+		return {
+			Cell{
+				name = 'Position',
+				children = {self:_toCellContent('class')}
+			},
+		}
+	end
+	return widgets
+end
+
+---@param key string
+---@return Widget?
+function CustomInjector:_toCellContent(key)
+	local args = self.caller.args
+	if not args[key] or args[key] == '' then return end
+	local iconData = PositionIcon[args[key]:lower()]
+	return iconData and Html.Fragment{
+		children = {
+			IconImageWidget{
+				imageLight = iconData.icon,
+				link = iconData.link,
+				size = '15px'
+			},
+			' ',
+			iconData.displayName
+		}
+	} or nil
 end
 
 ---@param lpdbData table
