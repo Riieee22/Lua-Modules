bff4ca72c41f223350bef920adfa0d738f7118df
Riieee <88617169+Riieee22@users.noreply.github.com>
2026-07-11 12:46:07 +0700

    Enhance CustomInjector with position widget support
    Added position handling in CustomInjector for character infobox.

--- lua/wikis/deltaforce/Infobox/Character/Custom.lua
+++ lua/wikis/deltaforce/Infobox/Character/Custom.lua
@@ -8,11 +8,17 @@
 local Lua = require('Module:Lua')
 
 local Class = Lua.import('Module:Class')
+local Logic = Lua.import('Module:Logic')
+local String = Lua.import('Module:StringUtils')
 
 local Injector = Lua.import('Module:Widget/Injector')
 local Character = Lua.import('Module:Infobox/Character')
+local PositionIcon = Lua.import('Module:PositionIcon', { loadData = true })
 
 local Widgets = Lua.import('Module:Widget/All')
+local HtmlWidgets = Lua.import('Module:Widget/Html/All')
+local Cell = Widgets.Cell
+local IconImageWidget = Lua.import('Module:Widget/Image/Icon/Image')
 
 ---@class DeltaforceCharacterInfobox: CharacterInfobox
 local CustomCharacter = Class.new(Character)
@@ -29,11 +35,45 @@ function CustomCharacter.run(frame)
 	return character:createInfobox()
 end
 
+---@param id string
+---@param widgets Widget[]
+---@return Widget[]
+function CustomInjector:parse(id, widgets)
+	if id == 'role' then
+		return {
+			Cell{
+				name = 'Position',
+				children = { self:_toCellContent('position') }
+			},
+		}
+	end
+	return widgets
+end
+
+---@param key string
+---@return Widget?
+function CustomInjector:_toCellContent(key)
+	local args = self.caller.args
+	if String.isEmpty(args[key]) then return end
+	local iconData = PositionIcon[args[key]:lower()]
+	return Logic.isNotEmpty(iconData) and HtmlWidgets.Fragment{
+		children = {
+			IconImageWidget{
+				imageLight = iconData.icon,
+				link = iconData.link
+			},
+			' ',
+			iconData.displayName
+		}
+	} or args[key]
+end
+
 ---@param lpdbData table
 ---@param args table
 ---@return table
 function CustomCharacter:addToLpdb(lpdbData, args)
 	lpdbData.extradata.class = args.class
+	lpdbData.extradata.position = args.position
 	return lpdbData
 end
 
